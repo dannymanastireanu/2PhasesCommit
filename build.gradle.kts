27fa0260a47b14c6af2c0d336aedc8aa47b86f8e
Dany Manastireanu <dannymanastireanu@gmail.com>
2023-04-04 23:00:46 -0700

    Add all

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     kotlin("jvm") version "1.8.0"
-    application
 }
 
-group = "org.example"
+group = "two.phases.commit"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -20,8 +19,4 @@ tasks.test {
 
 kotlin {
     jvmToolchain(11)
-}
-
-application {
-    mainClass.set("MainKt")
 }
