b897fe06763b266c8bfde13e32eb7897479c2ff3
Dany Manastireanu <dannymanastireanu@gmail.com>
2023-04-08 07:28:54 -0700

    Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.8.0"
+  kotlin("jvm") version "1.8.0"
+  application
 }
 
 group = "two.phases.commit"
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+  implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.8.20")
+  testImplementation(kotlin("test"))
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 kotlin {
-    jvmToolchain(11)
+  jvmToolchain(11)
+}
+application {
+  mainClass.set("MainKt")
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions.jvmTarget = "17"
 }
 
 tasks.named<Jar>("jar") {
-    archiveBaseName.set("2pc")
-    manifest {
-        attributes(
-            "Main-Class" to "MainKt"
-        )
-    }
+  archiveBaseName.set("2pc")
+  manifest {
+    attributes(
+      "Main-Class" to "MainKt"
+    )
+  }
 }
